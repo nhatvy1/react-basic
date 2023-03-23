8b80a496fcb96470b1f53b532e91d50bdc19c484
nhatvy1 <86657972+nhatvy1@users.noreply.github.com>
2023-03-24 00:20:54 +0700

    create first component

--- src/views/App.js
+++ src/views/App.js
@@ -1,27 +1,38 @@
-import logo from './logo.svg';
-import './App.scss'
 import React from 'react'
+import logo from './logo.svg'
+import './App.scss'
+import MyComponent from './Example/MyComponent'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world with ReactJS Hỏi Dân IT
-        </p>
-        <h1 className="demo">Huỳnh Nhật Vỹ</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function formatName(user) {
+	return user.firstName + " "+  user.lastName
+}
+
+const user = {
+	firstName: 'Vy',
+	lastName: 'Huynh'
+}
+
+const App = () => {
+	return (
+	<div className="App">
+		<header className="App-header">
+		<img src={logo} className="App-logo" alt="logo" />
+		<p>
+			Hello world with ReactJS Hỏi Dân IT
+		</p>
+		<h1>Xin chao { formatName(user) }</h1>
+		<MyComponent />
+		<a
+			className="App-link"
+			href="https://reactjs.org"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			Learn React
+		</a>
+		</header>
+	</div>
+	)
 }
 
 export default App;
---
+++ src/views/Example/MyComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>Hello My Component Hoi Dan IT</div>
+        )
+    }
+}
+
+export default MyComponent
